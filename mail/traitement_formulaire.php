9c2a04a850380e5f2f04a94625d204eed1a978ac
Mathieu PINSON <mpinson@e1r12p11.42.fr>
2017-08-17 15:26:12 +0200

    ok

---
+++ mail/traitement_formulaire.php
@@ -0,0 +1,93 @@
+    <?php
+    $email_destinataire = 'mpinson.pro@gmail.com';
+    $copie = 'non'; // 'oui' ou 'non'
+     
+    $message_envoye = "Votre message nous est bien parvenu !";
+    $message_non_envoye = "L'envoi du mail a échoué, veuillez réessayer SVP.";
+     
+    $message_erreur_formulaire = "Vous devez d'abord <a href=\"index.html\">envoyer le formulaire</a>.";
+    $message_formulaire_invalide = "Vérifiez que tous les champs soient bien remplis et que l'email soit sans erreur.";
+     
+    if (!isset($_POST['envoi']))
+    {
+        echo '<p>'.$message_erreur_formulaire.'</p>'."\n";
+    }
+    else
+    {
+        function Rec($text)
+        {
+            $text = htmlspecialchars(trim($text), ENT_QUOTES);
+            if (1 === get_magic_quotes_gpc())
+            {
+                $text = stripslashes($text);
+            }
+     
+            $text = nl2br($text);
+            return $text;
+        };
+     
+        function IsEmail($email)
+        {
+            $value = preg_match('/^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!\.)){0,61}[a-zA-Z0-9_-]?\.)+[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!$)){0,61}[a-zA-Z0-9_]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/', $email);
+            return (($value === 0) || ($value === false)) ? false : true;
+        }
+     
+        $nom     = (isset($_POST['nom']))     ? Rec($_POST['nom'])     : '';
+        $prenom  = (isset($_POST['prenom']))  ? Rec($_POST['prenom'])  : '';
+        $email   = (isset($_POST['email']))   ? Rec($_POST['email'])   : '';
+        $objet   = (isset($_POST['objet']))   ? Rec($_POST['objet'])   : '';
+        $message = (isset($_POST['message'])) ? Rec($_POST['message']) : '';
+     
+        $email = (IsEmail($email)) ? $email : '';
+     
+        if (($nom != '') && ($prenom != '') && ($email != '') && ($objet != '') && ($message != ''))
+        {
+            $headers  = 'MIME-Version: 1.0' . "\r\n";
+            $headers .= 'From:'.$nom.' '.$prenom .' <'.$email.'>' . "\r\n" .
+                    'Reply-To:'.$email. "\r\n" .
+                    'Content-Type: text/plain; charset="utf-8"; DelSp="Yes"; format=flowed '."\r\n" .
+                    'Content-Disposition: inline'. "\r\n" .
+                    'Content-Transfer-Encoding: 7bit'." \r\n" .
+                    'X-Mailer:PHP/'.phpversion();
+        
+            if ($copie == 'oui')
+            {
+                $cible = $email_destinataire.';'.$email;
+            }
+            else
+            {
+                $cible = $email_destinataire;
+            };
+     
+            $message = str_replace("&#039;","'",$message);
+            $message = str_replace("&#8217;","'",$message);
+            $message = str_replace("&quot;",'"',$message);
+            $message = str_replace('<br>','',$message);
+            $message = str_replace('<br />','',$message);
+            $message = str_replace("&lt;","<",$message);
+            $message = str_replace("&gt;",">",$message);
+            $message = str_replace("&amp;","&",$message);
+     
+            $num_emails = 0;
+            $tmp = explode(';', $cible);
+            foreach($tmp as $email_destinataires)
+            {
+                if (mail($email_destinataires, $objet, $message, $headers))
+                    $num_emails++;
+            }
+     
+            if ((($copie == 'oui') && ($num_emails == 2)) || (($copie == 'non') && ($num_emails == 1)))
+            {
+                echo '<p>'.$message_envoye.'</p>';
+            }
+            else
+            {
+                echo '<p>'.$message_non_envoye.'</p>';
+            };
+        }
+        else
+        {
+            echo '<p>'.$message_formulaire_invalide.' <a href="index.html">Retour au formulaire</a></p>'."\n";
+        };
+    };
+    ?>
